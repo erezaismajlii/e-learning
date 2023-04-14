3a03e4607ec9a355db933c7b1136d543f27a4b92
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-04-14 16:07:29 +0200

    Update Logout.js

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -3,3 +3,28 @@ function LogoutPage() {
   const dispatch = useDispatch();
   const { isAuth } = useSelector((state) => state.authReducer);
 
+  useEffect(() => {
+
+    (async function () {
+      try {
+        // const apiRes = await userApi.logout();
+        // if (apiRes.status === 200) {
+          dispatch(logout());
+          dispatch(setMessage('Logout successfully.', 'success'))
+          window.location.replace('/home');
+          // setTimeout(() => {
+          
+          // }, UX.DELAY_TIME);
+      //  }
+      } catch (error) {
+        dispatch(setMessage(error.response.data.message, 'error'));
+      }
+    })();
+
+    return () => {};
+  }, []);
+
+  return <>{isAuth && <GlobalLoading title="Logging out ..." />}</>;
+}
+
+export default LogoutPage;
