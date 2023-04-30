9d64210d36cd8e4700c76f6916a0aff7bf10bf87
Donat Aliu <donat.aliu@student.uni-pr.edu>
2023-05-01 01:57:17 +0200

    Update useCloseNavigation.js

---
+++ frontend/src/hooks/useCloseNavigation.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+
+function useCloseNavigation() {
+  useEffect(() => {
+    document.getElementById('englishNav').style.display = 'none';
+    return () => {
+      document.getElementById('englishNav')?.removeAttribute('style');
+    };
+  }, []);
+
+  return null;
+}
+
+export default useCloseNavigation;
