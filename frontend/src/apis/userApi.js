c8ebfb6062bf857de40c0556d5d9b0015156e74c
Dredhza Braina <94399125+DredhzaBraina@users.noreply.github.com>
2023-05-01 14:10:06 +0200

    Create userApi.js

---
+++ frontend/src/apis/userApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const URL = `/api/user`;
+const userApi = {
+  login: (email, password) => {
+    return axios.post(`${URL}/login`, { email, password });
+  },
+  register: (name, email, password) => {
+    return axios.post(`${URL}/register`, { name, email, password });
+  },
+  activationEmail: (activation_token) => {
+    return axios.post(`${URL}/activation`, { activation_token });
+  },
+   forgotPassword: (email) => {
+    return axios.post(`${URL}/forgot-password`, { email });
+  },
+  resetPassword: (password, access_token) => {
+    return axios.post(`${URL}/reset-password`, { password, access_token });
+  },
+  getToken: () => {
+    return axios.get(`${URL}/refresh-token`);
+  },
+  getUserInfo: () => {
+    return axios.get(`${URL}/get-user-info`);
+  },
+};
+  
