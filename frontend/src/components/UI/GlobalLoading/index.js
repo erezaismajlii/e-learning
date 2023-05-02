9e24c011aabffcd126b42e15f417da0c80be014c
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-05-02 20:56:35 +0200

    Update index.js

--- frontend/src/components/UI/GlobalLoading/index.js
+++ frontend/src/components/UI/GlobalLoading/index.js
@@ -1,1 +1,6 @@
+import PropTypes from 'prop-types';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import useToggleOverlay from '../../../hooks/useToggleOverlay';
+import React from 'react';
 
+function GlobalLoading({ title }) {
