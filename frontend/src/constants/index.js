f69f03f84d691218c681dc992c87a3be4babe0eb
Fleta Mujaj <fleta.mujaj@student.uni-pr.edu>
2023-05-04 23:00:45 +0200

    const.

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,9 @@
+export const ROUTES = {
+    HOME: '/home',
+    LISTENING_TOPICS: '/listening/topic',
+    GRAMMAR_LEVELS: '/grammar/level',
+    WORD_TOPIC: '/word',
+    IPA_LIST: '/ipa',
+    TEST: '/test',
+    GAMES_HOME: '/games'
+}
