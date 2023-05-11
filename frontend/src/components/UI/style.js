3935627952dbb18f24f57d2c012da4be0bd48053
Erëza Ismajli <94221195+erezaismajlii@users.noreply.github.com>
2023-05-11 02:21:41 +0200

    Update style.js

--- frontend/src/components/UI/style.js
+++ frontend/src/components/UI/style.js
@@ -1,31 +1,51 @@
+
 export const formStyle = () => ({
   root: {
-    padding: '1.5rem 4.0rem',
+    padding: '2.4rem 3.6rem',
     boxShadow: 'var(--box-shadow)',
     borderRadius: 'var(--border-radius)',
     width: '35rem',
+    backgroundColor: 'var(--bg-color-sec)',
+
     '& > *': {
-      marginTop: '0.8rem',
-      marginBottom: '0.8rem',
+      marginTop: '1.2rem',
+      marginBottom: '1.2rem',
     },
   },
 
   title: {
-    fontSize: '1.8rem',
+    fontSize: '2.4rem',
     fontWeight:'400px',
+    color: 'var(--text-color)',
   },
-  
+
+  labelIcon: {
+    fontSize: '3.6rem',
+    color: 'var(--text-color)',
+  },
+
   forgotPw: {
     color: 'var(--title-color)',
     opacity: 0.65,
     fontWeight: 500,
     fontSize: '1.4rem',
     textAlign: 'right',
+
+    '&:hover': {
+      opacity: 1,
+    },
   },
 
   icon: {
     fontSize: '1.8rem',
     color: 'var(--grey)',
     cursor: 'pointer',
+  },
+
+  visiblePw: {
+    color: 'var(--primary-color)',
+  },
+  visibleConfirmPw: {
+    color: 'var(--primary-color)'
   }
 });
