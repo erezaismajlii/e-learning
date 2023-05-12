d792e8f43049d761686439103a71ba15c7733224
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-05-12 23:25:14 +0200

    Update userService.js

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -43,3 +43,102 @@ exports.uploadAudio = async (audSrc, folderName ='', config = {}) => {
     throw error;
   }
 };
+
+exports.isExistWord = async (word = '', type = '') => {
+  try {
+    if (word === '' || type === '') {
+      return false;
+    }
+
+    return await WordModel.exists({word, type});
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.isExistSentence = async (sentence = '') => {
+  if (sentence === '') return false;
+  const newRegex = new RegExp(sentence, 'i');
+  return await SentenceModel.exists({ sentence: newRegex });
+};
+
+exports.getWordPack = async (
+  packInfo = {},
+  skip = 0,
+  limit = 10,
+  select = '',
+  sortType = null,
+  expandQuery = null,
+) => {
+  try {
+    let query = convertPackInfoToQueryStr(packInfo);
+
+    // add expand query
+    if (expandQuery && typeof expandQuery === 'object') {
+      Object.assign(query, expandQuery);
+    }
+
+    const packList = await WordModel.find(query)
+      .sort({ word: sortType })
+      .skip(skip)
+      .limit(limit)
+      .select(select);
+
+    return packList;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getWordTopicSlide = async (
+  packInfo = {}
+) => {
+  try {
+    let query = convertTopicsToQueryStr(packInfo);
+
+    const packList = await WordModel.find(query);
+      // .skip(skip)
+      // .limit(limit);
+
+    return packList;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getWordTopicGallery = async (
+  packInfo = {},
+  skip = 0,
+  limit = 1000,
+) => {
+  try {
+    let query = convertTopicsToQueryStr(packInfo);
+
+    const packList = await WordModel.find(query)
+      .skip(skip)
+      .limit(limit);
+
+    return packList;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+exports.countWordPack = async (packInfo = {}) => {
+  try {
+    let query = convertPackInfoToQueryStr(packInfo);
+    return await WordModel.countDocuments(query);
+  } catch (error) {
+    throw error;
+  }
+};
+
+  exports.countWordPackTopic = async (packInfo = {}) => {
+    try {
+      let query = convertTopicsToQueryStr(packInfo);
+      return await WordModel.countDocuments(query);
+    } catch (error) {
+      throw error;
+    }
+  };
