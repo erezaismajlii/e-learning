cf660ddd91b211caff0405a256230e36dd51aa96
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-05-12 23:44:32 +0200

    Update wordService.js

--- backend/services/wordService.js
+++ backend/services/wordService.js
@@ -69,3 +69,93 @@ exports.getWordDetail = async (id) => {
     throw error;
   }
 };
+exports.getWordByWord = async (word) => {
+  try {
+    const res = await WordModel.findOne({ word: word });
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.deleteWord = async (word = "", type = "") => {
+  try {
+    const isExist = await WordModel.findOne({ word, type });
+    const res = await WordModel.findByIdAndDelete(isExist._id);
+    if (res) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getFavoriteList = async (rawFavorites = []) => {
+  try {
+    if (!Array.isArray(rawFavorites) || rawFavorites.length === 0) {
+      return [];
+    }
+
+    let list = [];
+    for (let word of rawFavorites) {
+      const regex = new RegExp(`^${word}.*`, "gi");
+      const wordDetails = await WordModel.findOne({ Word: regex }).select(
+        "-_id type word mean phonetic picture"
+      );
+      if (wordDetails) {
+        list.push(wordDetails);
+      }
+    }
+
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getWordByTopic = async (topic) => {
+  try {
+    // var query = new RegExp( `^${topic}.*`,'gi');
+    const list = await WordModel.find({ Topics: topic });
+    if (list.length == 0) {
+      return null;
+    }
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getWordTopics = async () => {
+  try {
+    const list = await WordModel.distinct("Topics");
+    if (list.length == 0) {
+      return null;
+    }
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.getWordTopicByPage = async (
+  topic = "",
+  skip = 0,
+  limit = 1000,
+  select = ""
+) => {
+  try {
+    // let query = convertPackInfoToQueryStr(packInfo);
+    const packList = await WordModel.find({ topics: topic })
+      .skip(skip)
+      .limit(limit)
+      .select(select);
+
+    //  console.log(packList)
+    return packList;
+  } catch (error) {
+    throw error;
+  }
+};
