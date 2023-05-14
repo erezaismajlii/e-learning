9c858b27887d25e04f6362531122114e90ee3a6e
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-05-14 22:13:55 +0200

    Update quizService.js

--- backend/services/quizService.js
+++ backend/services/quizService.js
@@ -57,7 +57,7 @@ exports.deleteQuizByListenId = async (listenId = "") => {
   }
 };
 
-//get all
+//get all 
 exports.getAllQuizzes = async () => {
   try {
     const list = await QuizModel.find({});
