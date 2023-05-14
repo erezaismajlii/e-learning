20d0d218000dbbd3a5fba75d30531b1cba56db6f
erezaismajlii <erezaismajli@gmail.com>
2023-05-14 17:41:21 +0200

    Add ForgotPassword.js

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,15 +1,16 @@
 export const ROUTES = {
-    HOME: '/home',
-    REGISTER: '/register',
-    FORGOT_PASSWORD: '/forgot-password',
-    LISTENING_TOPICS: '/listening/topic',
-    GRAMMAR_LEVELS: '/grammar/level',
-    WORD_TOPIC: '/word',
-    IPA_LIST: '/ipa',
-    TEST: '/test',
-    GAMES_HOME: '/games'
-}
+  HOME: "/home",
+  REGISTER: "/register",
+  FORGOT_PASSWORD: "/forgot-password",
+  LISTENING_TOPICS: "/listening/topic",
+  GRAMMAR_LEVELS: "/grammar/level",
+  WORD_TOPIC: "/word",
+  IPA_LIST: "/ipa",
+  TEST: "/test",
+  GAMES_HOME: "/games",
+};
 
 export const UX = {
   DELAY_TIME: 1500,
+  DELAY_ANSWER: 3000,
 };
---
+++ frontend/src/redux/actions/messageAction.js
@@ -0,0 +1,17 @@
+import MESSAGE_CONSTANT from "../constants/messageConstant";
+
+export const setMessage = (message, type) => {
+  return {
+    type: MESSAGE_CONSTANT.SET_MESSAGE,
+    payload: {
+      message,
+      type,
+    },
+  };
+};
+
+export const closeMessage = () => {
+  return {
+    type: MESSAGE_CONSTANT.CLOSE_MESSAGE,
+  };
+};
