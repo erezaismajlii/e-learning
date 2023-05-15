d12724168262167b66a64da49f154f59daf0378a
Donat Aliu <donat.aliu@student.uni-pr.edu>
2023-05-15 05:41:42 +0200

    Create submitQuizModel.js

---
+++ backend/models/Quizzes/submitQuizModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SubmitQuizModel = new Schema({
+  Core: {
+    type: Number,
+    default: 0,
+  },
+
+  Comment: {
+    type: String,
+    maxlength: 200,
+  },
+
+  
+
+  IsFinish: {
+    type: Date,
+  },
+  
+
+  QuizId: {
+    type: Schema.Types.ObjectId,
+    ref: 'quiz',
+    required: true,
+  },
+
+  UserId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+
+ Answer: [{ type: Schema.Types.Map}]
+});
+
+module.exports = mongoose.model('submitquiz', SubmitQuizModel);
