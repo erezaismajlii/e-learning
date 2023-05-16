8eb5e74c7e4ae58566b32998ce289e0f3410135d
erezaismajlii <erezaismajli@gmail.com>
2023-05-17 01:40:18 +0200

    Created//Updated grammar page

--- frontend/src/components/GrammarAdmin/CreateGrammar/data.js
+++ frontend/src/components/GrammarAdmin/CreateGrammar/data.js
@@ -1,82 +1,89 @@
-import grammarApi from "apis/grammarApi";
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { setMessage } from "redux/actions/messageAction";
+import grammarApi from 'apis/grammarApi';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import CreateGrammar from './index';
+import { setMessage } from 'redux/actions/messageAction';
 
-const analysisLinkVideo = (linkVideo = "") => {
-  if (typeof linkVideo !== "string" || linkVideo === "") {
+const analysisLinkVideo = (linkVideo = '') => {
+  if (typeof linkVideo !== 'string' || linkVideo === '') {
     return null;
   }
 
-  let checkVid = linkVideo.includes("https://www.youtube.com");
-  if (!checkVid) {
-    checkVid = linkVideo.includes("youtu.be/");
-    if (checkVid) {
-      return linkVideo;
-    }
-    return null;
+  let checkVid = linkVideo.includes("https://www.youtube.com")
+  if(!checkVid){
+      checkVid =linkVideo.includes("youtu.be/")
+      if(checkVid)
+      {
+        return linkVideo;
+      }
+      return null;
   }
   return linkVideo;
 };
+
 
 function CreateGrammarData() {
   const [submitting, setSubmitting] = useState(false);
   const dispatch = useDispatch();
 
+
   const handleSubmit = async (data) => {
     try {
       setSubmitting(true);
       const { VidUpload, LinkVideo, ...rest } = data;
-
-      if (VidUpload && LinkVideo && LinkVideo.trim() != "") {
-        dispatch(
-          setMessage("Choose one of two options to add a video", "warning")
-        );
+      
+      if (VidUpload && LinkVideo && LinkVideo.trim()!=""){
+        dispatch(setMessage("Choose one of two options to add a video", "warning"));
         setSubmitting(false);
         return;
       }
 
-      let dataSend = [];
-      if (VidUpload == null) {
-        if (LinkVideo || LinkVideo.trim() != "") {
-          console.log(data)
+      let dataSend = []
+      if(VidUpload == null){
+          if(LinkVideo || LinkVideo.trim()!='' ){
+        //  console.log(data)
           const videoUrl = analysisLinkVideo(LinkVideo);
-          if (videoUrl == null) {
+          if (videoUrl==null) {
             dispatch(setMessage("Link video is invalid.", "warning"));
             setSubmitting(false);
             return;
           }
-          dataSend = {
+          dataSend ={
             ...rest,
-            Video: videoUrl,
+          Video: videoUrl,
           };
-        } else {
-          dataSend = {
+        }
+        else{
+          dataSend ={
             ...rest,
           };
         }
-      } else {
-        dataSend = {
-          ...rest,
-          Video: VidUpload,
-        };
       }
-      console.log(dataSend);
+      else{
+        dataSend ={
+          ...rest,
+         Video: VidUpload,
+        };  
+      }  
+      console.log(dataSend);   
       const apiRes = await grammarApi.postGrammar(dataSend);
 
       if (apiRes.status === 200) {
         dispatch(setMessage("Created grammar successfully", "success"));
         setSubmitting(false);
-      }
+     }
+
     } catch (error) {
-      const message =
-        error.response?.data?.message || "Error, can not create listening.";
-      dispatch(setMessage(message, "error"));
+      const message =  error.response?.data?.message ||
+      'Error, can not create listening.';
+        dispatch(setMessage(message, "error"));
       setSubmitting(false);
     }
   };
 
-  return;
+  return (
+    <CreateGrammar onSubmitForm={handleSubmit} submitting={submitting} />
+  );
 }
 
 export default CreateGrammarData;
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,30 @@
+export const COLOR_VAR_KEYS = [
+  { key: "--primary-color", label: "Primary" },
+  { key: "--secondary-color", label: "Secondary" },
+  { key: "--accent-color", label: "Accent" },
+  { key: "--bg-color-main", label: "Background primary" },
+  { key: "--bg-color-sec", label: "Background secondary" },
+  { key: "--bg-color-accent", label: "Background accent" },
+  { key: "--hover-color", label: "Hover color" },
+  { key: "--title-color", label: "Title" },
+  { key: "--text-color", label: "Text" },
+  { key: "--phonetic-color", label: "Phonetic" },
+];
+
+export const VOICE_KEYS = {
+  LS_KEY: 'voice',
+};
+
+export const DEFAULTS = {
+  VOICE_URI: 'Google US English',
+  VOICE_SPEED: 1,
+  VOICE_VOLUME: 1,
+};
+
+export const THEME_KEYS = {
+  PALETTE_KEY: 'palettes',
+};
+
 export const ROUTES = {
   HOME: "/home",
   REGISTER: "/register",
