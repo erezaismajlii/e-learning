16085b111a3bfba2e850343693369d4775c90f57
Donat Aliu <donat.aliu@student.uni-pr.edu>
2023-05-18 21:58:33 +0200

    Create authorizationMiddleware.js
    This commit adds three middleware functions (checkAdmin, checkInstructor, and checkAccess) for role-based access control

---
+++ backend/middlewares/authorizationMiddleware.js
@@ -0,0 +1,44 @@
+const Users = require("../models/userModel");
+exports.checkAdmin = async (req, res, next) => {
+
+  try {
+    const user = await Users.findOne({ _id: req.user.id });
+
+    if (user.roleType !== "admin")
+      return res
+        .status(500)
+        .json({ message: "Admin resources access denied." });
+
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+exports.checkInstructor = async (req, res, next) => {
+  try {
+    const user = await Users.findOne({ _id: req.user.id });
+
+    if (user.roleType !== "instructor") {
+      if (user.roleType !== "admin") {
+        return res
+          .status(500)
+          .json({ message: "Instructor resources access denied." });
+      }
+    }
+
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+exports.checkAccess = async (req, res, next) => {
+  try {
+    const user = await Users.findOne({ _id: req.user.id });
+    if (user.roleType === "user")
+      return res.status(500).json({ message: "Access denied" });
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
