b2c95cacd4e6b160fecc4c79d7033210d567d95a
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-05-18 21:56:42 +0200

    Update grammarService.js

--- backend/services/grammarService.js
+++ backend/services/grammarService.js
@@ -34,3 +34,92 @@ exports.getGrammarById = async (id = "") => {
     throw error;
   }
 };
+
+exports.getGrammarByListenId = async (listenId = "") => {
+  try {
+    const res = await GrammarModel.find({ ListeningId: listenId }).populate(
+      "items"
+    );
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//delete by id
+exports.deleteGrammarById = async (id = "") => {
+  try {
+    const res = await GrammarModel.findByIdAndUpdate(id, { isLocked: -1 });
+    if (res) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//delete by listenid
+exports.deleteGrammarByListenId = async (listenId = "") => {
+  try {
+    const res = await GrammarModel.deleteOne({ ListeningId: listenId });
+    if (res) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//get grammar by level
+exports.getGrammarByLevel = async (level) => {
+  try {
+    // var query = new RegExp( `^${topic}.*`,'gi');
+    const list = await GrammarModel.find({ Level: level });
+    if (list.length == 0) {
+      return null;
+    }
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//get levels
+exports.getGrammarLevels = async () => {
+  try {
+    const list = await GrammarModel.distinct("Level");
+    if (list.length == 0) {
+      return null;
+    }
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//get all
+exports.getAllGrammars = async () => {
+  try {
+    const list = await GrammarModel.find({});
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.searchGrammar = async (title = "", limit = 50, select = "") => {
+  try {
+    const regex = new RegExp(`^${title}.*`, "gi");
+    const list = await GrammarModel.find({ Title: regex })
+      .limit(limit)
+      .select(select);
+    if (list.length == 0) {
+      return null;
+    }
+    return list;
+  } catch (error) {
+    throw error;
+  }
+};
